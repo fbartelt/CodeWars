e3cd76d689abb0d65431b96872f5d8c9ac72e8d7
fbartelt <fofispirocudo22@gmail.com>
2020-07-25 21:58:38 -0300

    First 1kyu + esolang interpreter

--- Python/1kyu-regular_expression_for_binary_numbers_divisible_by_n.py
+++ Python/1kyu-regular_expression_for_binary_numbers_divisible_by_n.py
@@ -1,33 +1,72 @@
 def regex_divisible_by(n):
-    ## PRA REGEX: percorrer dfa ao contrario e ir substituindo as keys?
+    if n==1:
+        return '^(0|1)+$'
     dfa = get_dfa(n)
-    for states in list(dfa.keys())[::-1]:
-        print(dfa[states])
+    while len(dfa) > 2:
+        current_state = list(dfa)[-1]
+        current_transitions = get_self_state(current_state, dfa[current_state])
+        dfa.pop(current_state,None)
+        for _, transitions in dfa.items():
+            if (current_state in transitions.keys()):
+                change_states(transitions, current_transitions, current_state)
+        dfa.pop(current_state,None)
+    state_one = list(dfa)[-1]
+    state_one = get_self_state(state_one, dfa[state_one])
+    regex = '^(?:0|1{})+$'.format(state_one['0'])
+    return regex
+
+def change_states(transitions, current_transitions, current_state):
+    holded = 0
+    middle = transitions[current_state]
+    for to_state, value in current_transitions.items():
+        if (to_state in transitions.keys()):
+            transitions[to_state] = '(?:{}|{}{})'.format(transitions.pop(to_state),middle ,value)
+        else:
+            value_hold = transitions.pop(current_state, '')
+            if value_hold != '':
+                holded = value_hold
+            value_ = '{}{}'.format(holded or value_hold, value)
+            transitions.setdefault(to_state, value_)
+    return transitions
+
+def get_self_state(state, to_states):
+    dicio = {}
+    self_state = ''
+    for key, value in to_states.items():
+        if key == state:
+            self_state = '(?:{})*'.format(value)
+        else:
+            dicio.setdefault(key, value)
+    dicio = {key : self_state+value for key,value in dicio.items()}
+    return dicio
 
 def get_dfa(n):
     ACCEPTING_STATE = START_STATE = '0'
     SYMBOL_0 = '0'
     dfa = {
-        str(states):{
-            str(symbol): 'to_state' for symbol in range(2)
-        }
+        str(states):{}
         for states in range(n)
     }
-    print(dfa)
-    dfa[START_STATE][SYMBOL_0] = ACCEPTING_STATE
-    # `lookup_table` keeps track: 'number string' -->[dfa]--> 'end_state'
-    #lookup_table = { SYMBOL_0: ACCEPTING_STATE }.setdefault
+    dfa[SYMBOL_0].setdefault(ACCEPTING_STATE,START_STATE)
     lo = [str(a // 2) for a in range(2 * n)]
-    print(lo)
     for num in range(2 * n):
         end_state = str(num % n)
         binary = bin(num)
         before_end_state = lo[num]
-        print(lo[num], binary, end_state)
-        dfa[before_end_state][binary[-1]] = end_state
-        print(dfa)
-        #lookup_table(num_s, end_state)
-    print(dfa)
+        dfa[before_end_state].setdefault(end_state,binary[-1])
     return dfa
 
-regex_divisible_by(5)
+##### TEST FUNCTION
+import re
+def check_func(n):
+    for i in range(1,n):
+        pattern = re.compile(regex_divisible_by(i))
+        lista = [x for x in range(1000) if not x%i ]
+        lista2 = [x for x in range(1000) if re.search(pattern, bin(x)[2:])]
+        print('PAT', pattern)
+        if lista == lista2:
+            print(i, 'OK')
+        else:
+            print(i, 'BAD')
+#check_func(19)
+print(regex_divisible_by(11))
--- README.md
+++ README.md
@@ -2,4 +2,58 @@
 
 This repository is just for storing every new kata I solve. Aiming to get more knowledge on Python, Elixir and Go Currently trying to do a kata a day in Python
 
-![](https://www.codewars.com/users/BabiRuiva/badges/large)
+[![](https://www.codewars.com/users/BabiRuiva/badges/large)](https://www.codewars.com/users/BabiRuiva)
+
+<p align="center">
+	<a href="https://www.codewars.com/users/BabiRuiva">
+  	 <img src="https://www.codewars.com/users/BabiRuiva/badges/large"/> 
+  	</a>
+</p>
+## Amount of completed Katas per Language
+
+| Language | Total |
+| :------: | :---: |
+|  Python  |  32   |
+|  Elixir  |  23   |
+| Haskell  |   5   |
+
+## Amount of completed Katas per Kyu
+
+### Python
+
+| Kyu  | Total |
+| :--: | :---: |
+|  1   |   1   |
+|  2   |   2   |
+|  3   |   3   |
+|  4   |   8   |
+|  5   |   3   |
+|  6   |  11   |
+|  7   |   3   |
+|  8   |   1   |
+
+### Elixir
+
+| Kyu  | Total |
+| :--: | :---: |
+|  1   |   -   |
+|  2   |   -   |
+|  3   |   1   |
+|  4   |   8   |
+|  5   |   4   |
+|  6   |   5   |
+|  7   |   2   |
+|  8   |   3   |
+
+### Haskell
+
+| Kyu  | Total |
+| :--: | :---: |
+|  1   |   -   |
+|  2   |   -   |
+|  3   |   -   |
+|  4   |   -   |
+|  5   |   -   |
+|  6   |   3   |
+|  7   |   -   |
+|  8   |   2   |
